9bbcbedec030e84ee2826af782cf6a149db5f464
Madhur Shrimal <madhur.shrimal@gmail.com>
2015-08-11 01:52:34 -0400

    bug fix

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,11 +1,13 @@
 public class BinarySearch{
 	public static void main(String[] args){
-		int[] a = {1, 3, 5, 6, 7, 10, 11, 15};
+		int[] a = {-5};
 		int len = a.length;
 		System.out.println(binSearch(a, Integer.parseInt(args[0]), 0, len));
 	}
 	public static boolean binSearch(int[] a, int element, int low, int high){
 		if(low + 1 == high){
+			if(a[low] == element)
+				return true;
 			return false;
 		}else{
 			int mid = (low + high)/2;
